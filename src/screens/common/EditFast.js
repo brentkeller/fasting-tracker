b6a7f771b5600feb2973885ff2a9d00072d22c3b
Brent Keller <brentkeller@gmail.com>
2018-12-28 00:58:22 -0500

    WIP EditFast

---
+++ src/screens/common/EditFast.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+import { Modal, TouchableHighlight, View, Alert } from 'react-native';
+
+import {
+  Form,
+  Item,
+  Label,
+  Input,
+  CardItem,
+  Card,
+  Left,
+  Right,
+  Button,
+} from 'native-base';
+
+import { getDurationString } from 'common/date';
+
+const EditFast = ({ fast, onCancel, onSave }) => {
+  if (!fast) return null;
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={fast != null}
+      onRequestClose={() => {
+        Alert.alert('Modal has been closed.');
+      }}
+    >
+      <View style={{ marginTop: 22 }}>
+        <Card>
+          <CardItem header bordered>
+            <Text>Edit fast</Text>
+          </CardItem>
+          <CardItem>
+            <Form>
+              <Item>
+                <Label>Start</Label>
+                <Input />
+              </Item>
+            </Form>
+          </CardItem>
+          <CardItem footer>
+            <Left>
+              <Button
+                transparent
+                onPress={() => {
+                  onCancel();
+                }}
+              >
+                <Text>Cancel</Text>
+              </Button>
+            </Left>
+            <Right>
+              <Button transparent>
+                <Text>Save</Text>
+              </Button>
+            </Right>
+          </CardItem>
+        </Card>
+      </View>
+    </Modal>
+  );
+};
+
+EditFast.propTypes = {
+  fast: PropTypes.object.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+};
+
+export default EditFast;
--- src/screens/common/FastList.js
+++ src/screens/common/FastList.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { deleteFast } from 'common/state/fasts/fasts';
 import { ListView, Alert } from 'react-native';
 import { Button, Icon, List } from 'native-base';
-import FastListItem from '../common/FastListItem';
+import FastListItem from './FastListItem';
+import EditFast from './EditFast';
 
 class FastList extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {};
+
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.deleteRow = this.deleteRow.bind(this);
     this.editRow = this.editRow.bind(this);
+    this.cancelEdit = this.cancelEdit.bind(this);
+    this.saveEdit = this.saveEdit.bind(this);
   }
 
   editRow(data) {
-    this.props.navigation.navigate({ routeName: 'edit', params: data.id });
+    this.setState({ editFast: { ...data } });
+    // this.props.navigation.navigate({ routeName: 'edit', params: data.id });
   }
 
   deleteRow(data) {
@@ -31,29 +37,47 @@ class FastList extends React.Component {
     ]);
   }
 
+  cancelEdit() {
+    this.setState({ editFast: null });
+  }
+
+  saveEdit() {
+    console.log('saving', this.state.editFast);
+  }
+
   render() {
     const { fasts } = this.props;
+    const { editFast } = this.state;
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2,
     });
 
     return (
-      <List
-        leftOpenValue={75}
-        rightOpenValue={-75}
-        dataSource={ds.cloneWithRows(fasts)}
-        renderRow={data => <FastListItem fast={data} />}
-        renderLeftHiddenRow={data => (
-          <Button full onPress={() => this.editRow(data)}>
-            <Icon active name="edit" type="MaterialIcons" />
-          </Button>
+      <Fragment>
+        <List
+          leftOpenValue={75}
+          rightOpenValue={-75}
+          dataSource={ds.cloneWithRows(fasts)}
+          renderRow={data => <FastListItem fast={data} />}
+          renderLeftHiddenRow={data => (
+            <Button full onPress={() => this.editRow(data)}>
+              <Icon active name="edit" type="MaterialIcons" />
+            </Button>
+          )}
+          renderRightHiddenRow={data => (
+            <Button full danger onPress={() => this.deleteRow(data)}>
+              <Icon active name="trash" />
+            </Button>
+          )}
+        />
+        {editFast && (
+          <EditFast
+            fast={editFast}
+            onCancel={() => this.cancelEdit()}
+            onSave={() => this.saveEdit()}
+          />
         )}
-        renderRightHiddenRow={data => (
-          <Button full danger onPress={() => this.deleteRow(data)}>
-            <Icon active name="trash" />
-          </Button>
-        )}
-      />
+      </Fragment>
     );
   }
 }
