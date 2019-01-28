c730b704d5aa83986a7e8e220a689d6421228835
Brent Keller <brentkeller@gmail.com>
2019-01-27 21:20:22 -0500

    Tidy up Navigator

--- src/screens/Navigator.js
+++ src/screens/Navigator.js
@@ -3,56 +3,7 @@ import Home from './home/Home';
 import History from './history/History';
 import Settings from './settings/Settings';
 import Stats from './stats/Stats';
-
-import { Platform, ScrollView, StatusBar, Text } from 'react-native';
-import {
-  createAppContainer,
-  createStackNavigator,
-  createDrawerNavigator,
-  SafeAreaView,
-} from 'react-navigation';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-
-// const MyNavScreen = ({ navigation, banner }) => (
-//   <ScrollView>
-//     <SafeAreaView forceInset={{ top: 'always' }}>
-//       <Text>{banner}</Text>
-//       <Button onPress={() => navigation.openDrawer()} title="Open drawer" />
-//       <Button
-//         onPress={() => navigation.navigate('Home')}
-//         title="Open other screen"
-//       />
-//       <Button onPress={() => navigation.navigate('Index')} title="Go back" />
-//     </SafeAreaView>
-//     <StatusBar barStyle="default" />
-//   </ScrollView>
-// );
-
-// const InboxScreen = ({ navigation }) => (
-//   <MyNavScreen banner={'Inbox Screen'} navigation={navigation} />
-// );
-// InboxScreen.navigationOptions = {
-//   headerTitle: 'Inbox',
-// };
-
-// const InboxStack = createStackNavigator(
-//   {
-//     Inbox: { screen: InboxScreen },
-//     Email: { screen: EmailScreen },
-//   },
-//   {
-//     navigationOptions: {
-//       drawerLabel: 'Inbox',
-//       drawerIcon: ({ tintColor }) => (
-//         <MaterialIcons
-//           name="move-to-inbox"
-//           size={24}
-//           style={{ color: tintColor }}
-//         />
-//       ),
-//     },
-//   },
-// );
+import { createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 const DrawerNavigator = createDrawerNavigator(
   {
@@ -64,10 +15,10 @@ const DrawerNavigator = createDrawerNavigator(
       path: 'History',
       screen: History,
     },
-    Stats: {
-      path: 'Stats',
-      screen: Stats,
-    },
+    // Stats: {
+    //   path: 'Stats',
+    //   screen: Stats,
+    // },
     Settings: {
       path: 'Settings',
       screen: Settings,
